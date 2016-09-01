1d5e7da87c936cc0211731cda3088603f223fc03
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2016-09-01 08:44:47 -0700

    cactus scripts moved to this repo

---
+++ bin/cactus_runner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+from time import gmtime, strftime
+import os
+import subprocess
+import utils
+	
+def run(config, dir, outputhal):
+    utils.check_existence_or_raise(config)
+    utils.create_dir_if_not_exists(dir)
+    command = ['runProgressiveCactus.sh', '--batchSystem', 'parasol', '--bigBatchSystem', 'singleMachine',
+    '--defaultMemory', '8589934593', '--bigMemoryThreshold', '8589934592', '--bigMaxMemory', '893353197568',
+    '--bigMaxCpus', '25', '--maxThreads', '25', '--parasolCommand=/cluster/home/jcarmstr/bin/parasol -host=ku',
+    '--retryCount', '3']
+    params = command + [config, dir, outputhal]
+    print params
+    subprocess.check_call(params)
+    return outputhal
+	
