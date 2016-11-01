cae20f4e07720758f43f6266d4cd79c56d5ae300
Ksenia Krasheninnikova <ksenia@kolossus.sdsc.edu>
2016-11-01 05:17:55 -0700

    working version of blocks plotting

--- bin/synteny_blocks/synteplot.py
+++ bin/synteny_blocks/synteplot.py
@@ -4,13 +4,15 @@ import os
 import sys
 import matplotlib
 import argparse
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.patches import Circle, Wedge, Polygon, Rectangle
 from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
 from model import Entry
 from color_cache import cache
+
+length_threshold = 1000000
 
 def parse_genome(genome_path):
     chrs = []
@@ -30,42 +32,54 @@ def parse_genome(genome_path):
                 names.add(e.seq_id.split('.')[1])
     return chrs
 
-def check_if_homolog_to_reference_chrom(c, reference_block_id2colors_sizes):
+def check_if_homolog_to_reference_chrom(c, ref_blocks):
     homologous_blocks_len = 0
     c_hom = []
     for e in c:
-        if e.block_id in reference_block_id2colors_sizes.keys():
+        if e.block_id in ref_blocks and e.length >= length_threshold:
             homologous_blocks_len += e.length
             c_hom.append((e,True))
         else:
             c_hom.append((e,False))
     return float(homologous_blocks_len) / sum(map(lambda x: x.length, c)) >= 0.5, c_hom
 
+class ReferenceBlockDrawing:
+    def __init__(self,e, color, y_size, y_start):
+        self.e = e
+        self.color = color
+        self.y_size = y_size
+        self.y_start = y_start
+
 #genomes is the list of chrs (chrs is a list of chromosomes of a genome)
-def karyoplot(ref_genome, genomes):
-    fig = plt.figure(1,figsize=(8, 6))
-    ax = fig.add_subplot(111, aspect='equal')
+def karyoplot(ref_genome, genomes,names, folder):
     default_color = 'black'
     breath = 0.05
-#    block_length_param = 0.0000001
-    #block_length_param = 7.5e-09
     block_length_param = 5e-08
     x_dist = 0.05
     y_dist = 0.05
-    x_start = x_dist
+    plt.hold(False) 
     for c in ref_genome:
-        reference_block_id2colors_sizes = {}
+        chr_name = c[0].seq_id.split('.')[1]
+        print 'processing',chr_name
+        fig = plt.figure(1,figsize=(5, 7))
+        ax = fig.add_subplot(111, aspect='equal')
+        ax.set_ylim([0,1.5])
+        x_start = x_dist
+        block_id2drawing = {}
         cache_names = cache.keys()
         color_i = 0
         y_start = y_dist
         for e in c:
+            if e.length < length_threshold:
+                continue
             if color_i == len(cache_names):
-                i=0
-                #raise Exception('No more colors!')
+                #i = 0
+                raise Exception('No more colors!')
             y_size = float(e.length) * block_length_param 
             r = Rectangle((x_start, y_start), breath, y_size, facecolor = cache[cache_names[color_i]])
+            print r
             #remember the colors in reference
-            reference_block_id2colors_sizes[e.block_id] = (cache[cache_names[color_i]],y_size)
+            block_id2drawing[e.block_id] = ReferenceBlockDrawing(e, cache[cache_names[color_i]], y_size, y_start)
             y_start += y_size
             #print r
             ax.add_patch(r)
@@ -79,37 +93,42 @@ def karyoplot(ref_genome, genomes):
         ax.add_patch(w1)
         ax.add_patch(w2)
         
-        for g in genomes:
+        
+        for g,name in zip(genomes,names):
             x_start += breath + x_dist
             y_start = y_dist
+            rectangles = []
             for c in g:
-                if_hom,c_hom = check_if_homolog_to_reference_chrom(c, reference_block_id2colors_sizes)
+                if_hom,c_hom = check_if_homolog_to_reference_chrom(c, block_id2drawing.keys())
                 if if_hom:
-                    #w2 = Wedge((center_x, y_start), radius, theta2, theta1, width=0.00001, facecolor='white', edgecolor='black')
-                    #ax.add_patch(w2)
                     for e,status in c_hom:
                         if status: #True if homologous
-                            r_color, r_y_size = reference_block_id2colors_sizes[e.block_id]
-                            r = Rectangle((x_start, y_start), breath, r_y_size, facecolor = r_color)
-                            y_start += r_y_size
+                            ref_drawing = block_id2drawing[e.block_id]
+                            r = Rectangle((x_start, y_start), breath, ref_drawing.y_size, facecolor = ref_drawing.color)
+                            rectangles.append(r)
+                            y_start += ref_drawing.y_size
                             ax.add_patch(r)
                         else:
-                            print 'else'
+                            if e.length < length_threshold:
+                                continue
+                            #print 'else'
                             y_size = float(e.length) * block_length_param 
                             r = Rectangle((x_start, y_start), breath, y_size, facecolor = default_color)
-                            y_start += r_y_size
+                            rectangles.append(r)
+                            y_start += y_size
                             ax.add_patch(r)
-                    #radius = breath/2.0
-                    #center_x = x_start + breath/2.0
-                    #w1 = Wedge((center_x, y_start), radius, theta1, theta2, width=0.00001, facecolor='white', edgecolor='black')
-                    #y_start += 2*radius
-                    #ax.add_patch(w1)
                 
                     y_start += radius
-        x_start += breath + x_dist                     
-        break
-    fig.savefig('test.png', dpi=90)
+            name = ' '.join(name.split('_'))
+            ax.text(x_start+breath/2.0, y_start, name, va='bottom', rotation='vertical')
 
+        x_start += breath + x_dist                     
+        ax.set_axis_off()
+        ax.set_title(chr_name)
+        fig.savefig(os.path.join(folder,chr_name+'.png'), dpi=90)
+        fig.clf()
+        plt.cla()
+        plt.close()
 '''
         #Plot semicircles at the beginning and end of the chromosomes
         center_x = x_start + (x_end-x_start)/2.0
@@ -143,6 +162,7 @@ if __name__ == '__main__':
     #        f = urllib.urlopen(url)
     #        k_file.write(f.read())
     parser = argparse.ArgumentParser()
+    parser.add_argument('path',help='folder for the output')
     parser.add_argument('reference')
     parser.add_argument('others', nargs='+')
     args = parser.parse_args()
@@ -150,11 +170,13 @@ if __name__ == '__main__':
     rg = parse_genome(args.reference)
     print 'loading other genomes..'
     gs = []
+    names = []
     for g in args.others:
+       names.append(g)
        x =  parse_genome(g)
        gs.append(x)
     print 'plotting...'
-    karyoplot(rg,gs)
+    karyoplot(rg,gs,names,args.path)
 
 
 
